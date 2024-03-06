252f2adf3a20dadc1359b739e1afdcc9260f4994
tnmtthw <fortunetamares@gmail.com>
2024-03-06 14:59:49 +0800

    lazy commit

--- routes/question_routes.py
+++ routes/question_routes.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, HTTPException
+import random
 from typing import List
 from models.question import Hugis, Kulay, Numero, Binasa, Napakinggan, Ponolohiya, Talasalitaan, Gramatika
 from database.connection import hugis_collection, kulay_collection, numero_collection, binasa_collection, napakinggan_collection, ponolohiya_collection, talasalitaan__collection, gramatika__collection
@@ -13,38 +14,48 @@ async def get_hugis():
 @router.get("/kulay", response_model=List[Kulay])
 async def get_kulay():
     kulay = list(kulay_collection.find({}, {"_id": 0}))
+    
+    # Shuffle the questions
+    random.shuffle(kulay)
+    
+    # Randomize options and option_images for each question
+    for question in kulay:
+        options = question["options"]
+        option_images = question["option_images"]
+        correct_index = question["correct_option_index"]
+        
+        # Ensure correct option index is within the first 4 options
+        if correct_index >= 4:
+            options = options[:3] + [options[correct_index]]
+            option_images = option_images[:3] + [option_images[correct_index]]
+            correct_index = 3
+        
+        # Construct list of indices for options and option_images
+        indices = list(range(len(options)))
+        
+        # Shuffle the indices
+        random.shuffle(indices)
+        
+        # If correct index is not within the first 4 options, place it randomly within them
+        if correct_index not in indices[:4]:
+            replace_index = random.randint(0, 3)
+            indices[replace_index] = correct_index
+        
+        # Update correct option index
+        question["correct_option_index"] = indices.index(correct_index)
+        
+        # Update question with shuffled options and option_images
+        question["options"] = [options[i] for i in indices[:4]]
+        question["option_images"] = [option_images[i] for i in indices[:4]]
+    
     return kulay
+
+# @router.get("/kulay", response_model=List[Kulay])
+# async def get_kulay():
+#     kulay = list(kulay_collection.find({}, {"_id": 0}))
+#     return kulay
 
 @router.get("/numero", response_model=List[Numero])
 async def get_numero():
     numero = list(numero_collection.find({}, {"_id": 0}))
     return numero
-
-@router.get("/binasa", response_model=List[Binasa])
-async def get_binasa():
-    binasa = list(binasa_collection.find({}, {"_id": 0})) 
-    return binasa
-
-@router.get("/napakinggan", response_model=List[Napakinggan]) 
-async def get_napakinggan():
-    napakinggan = list(napakinggan_collection.find({}, {"_id": 0})) 
-    return napakinggan
-
-@router.get("/ponolohiya", response_model=List[Ponolohiya]) 
-async def get_ponolohiya():
-    ponolohiya = list(ponolohiya_collection.find({}, {"_id": 0})) 
-    return ponolohiya
-
-@router.get("/talasalitaan", response_model=List[Talasalitaan]) 
-async def get_talasalitaan():
-    talasalitaan = list(talasalitaan__collection.find({}, {"_id": 0})) 
-    return talasalitaan
-
-@router.get("/gramatika", response_model=List[Gramatika]) 
-async def get_gramatika():
-    gramatika = list(gramatika__collection.find({}, {"_id": 0})) 
-    return gramatika
-
-
-
-
